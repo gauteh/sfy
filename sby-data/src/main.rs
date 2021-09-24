458d53a556b47fb31e568f08b4ac093bb39f6788
Gaute Hope <gaute.hope@met.no>
2021-09-24 15:35:21 +0200

    sby-data: skeleton for message listener

--- sby-data/src/main.rs
+++ sby-data/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate log;
+use env_logger::Env;
+
+use axum::{
+    handler::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    env_logger::Builder::from_env(Env::default().default_filter_or("warn,sby_data=debug")).init();
+
+    info!("Hello, world!");
+
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
