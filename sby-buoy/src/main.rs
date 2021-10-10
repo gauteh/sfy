14c7c559611497b99701f4852469f77831f640c1
Gaute Hope <gaute.hope@met.no>
2021-10-10 21:40:53 +0200

    i2c ping works + defmt print

--- sby-buoy/src/main.rs
+++ sby-buoy/src/main.rs
@@ -8,23 +8,59 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
                      // use panic_itm as _; // logs messages over ITM; requires ITM support
                      // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
-
 use ambiq_hal as hal;
-use hal::prelude::{*, halc::c_types::*};
 use cortex_m_rt::entry;
+use hal::prelude::*;
+use ufmt::uwriteln;
+use defmt::{Logger, Write, global_logger};
+use defmt::{error, warn, info, debug, trace};
+use core::ptr;
+
+use core::cell::RefCell;
 
 // mod note;
 
+pub struct UartLogger {
+    uart: hal::uart::Uart0
+}
+
+// unsafe impl Sync for UartLogger {}
+
+// impl stlog::GlobalLog for UartLogger {
+//     fn log(&self, address: u8) {
+//         if let Some(uart) = self.uart.borrow_mut().as_mut() {
+//             nb::block!(uart.write(address));
+//         }
+//     }
+// }
+
+#[global_logger]
+pub struct UartGlobalLogger;
+
+unsafe impl defmt::Logger for UartGlobalLogger {
+    fn acquire() -> Option<ptr::NonNull<dyn Write>> {
+        unsafe {
+            LOGGER.as_mut().map(|l| ptr::NonNull::new_unchecked(l as &mut dyn defmt::Write))
+        }
+    }
+
+    unsafe fn release(writer: ptr::NonNull<dyn Write>) {
+    }
+}
+
+impl defmt::Write for UartLogger {
+    fn write(&mut self, bytes: &[u8]) {
+        for b in bytes {
+            nb::block!(self.uart.write(*b));
+        }
+    }
+}
+
+static mut LOGGER: Option<UartLogger> = None;
+
+
 #[entry]
 fn main() -> ! {
-    let mut dp = hal::pac::Peripherals::take().unwrap();
-    let core = hal::pac::CorePeripherals::take().unwrap();
-
-    let pins = hal::gpio::Pins::new(dp.GPIO);
-
-    let mut delay = hal::delay::Delay::new(core.SYST, &mut dp.CLKGEN);
-    let mut serial = hal::uart::Uart0::new(dp.UART0, pins.tx0, pins.rx0);
-
     // Set the clock frequency.
     unsafe {
         halc::am_hal_clkgen_control(
@@ -40,18 +76,67 @@ fn main() -> ! {
         halc::am_bsp_low_power_init();
     }
 
+    let mut dp = hal::pac::Peripherals::take().unwrap();
+    let core = hal::pac::CorePeripherals::take().unwrap();
+
+    let pins = hal::gpio::Pins::new(dp.GPIO);
+
+    let mut delay = hal::delay::Delay::new(core.SYST, &mut dp.CLKGEN);
+    let serial = hal::uart::Uart0::new(dp.UART0, pins.tx0, pins.rx0);
+    unsafe { LOGGER = Some(UartLogger { uart: serial }); }
+
+    let mut i2c = hal::i2c::I2c::new(dp.IOM4, pins.d15, pins.d14);
+    // let note = note::Note::new(i2c);
+
     // Set up BSP leds
     let mut led = pins.d13.into_push_pull_output();
 
+    let mut i = 0;
+
+    let noteaddr = 0x17u8;
+
     // Blink forever
     loop {
-        ufmt::uwriteln!(&mut serial, "hello world\r").unwrap();
+        defmt::info!("hello world!");
+        // info!("hello world!");
+        // uwriteln!(&mut serial, "hello world: {}\r", i).unwrap();
+        i += 1;
 
         // Toggle LEDs
         led.toggle().unwrap();
 
         // Delay
         delay.delay_ms(300u32);
+
+        // uwriteln!(&mut serial, "scanning i2c bus:\r");
+        for a in 0x14u8..0x19u8 {
+            info!("trying: 0x{:02x}", a);
+            if a == 0x17 {
+                debug!("trying notecarrier");
+                // uwriteln!(&mut serial, "Trying: notecarrier (0x17)\r");
+            } else {
+                // uwriteln!(&mut serial, "Trying {}\r", a);
+            }
+            let r = i2c.write(a, &[]);
+            if r.is_ok() {
+                warn!("0x{:02x}: {:?}", a, r);
+            } else {
+                info!("0x{:02x}: {:?}", a, r);
+            }
+            // uwriteln!(&mut serial, "{}: {:?}\r", a, r);
+            delay.delay_ms(300u32);
+        }
+
+
+        // Write something to the noteboard
+
+        // i2c.write(noteaddr, r#"{"req": "card.time"}\n"#.as_bytes());
+
+        delay.delay_ms(300u32);
+
+        // let mut buffer = [0u8; 10];
+        // i2c.read(noteaddr, &mut buffer);
+        // ufmt::uwriteln!(&mut serial, "note: {}", unsafe { core::str::from_utf8_unchecked(&buffer) });
     }
 }
 
--- sby-buoy/src/note/mod.rs
+++ sby-buoy/src/note/mod.rs
@@ -1,27 +1,57 @@
-use serde::Deserialize;
+//! Protocol for transmitting: https://dev.blues.io/notecard/notecard-guides/serial-over-i2c-protocol/
+//! API: https://dev.blues.io/reference/notecard-api/introduction/
+//!
+//! Each
+use crate::hal;
 
-#[derive(Deserialize)]
-pub struct Status {
-    status: heapless::String<10>,
-    usb: bool,
-    storage: usize,
-    time: u64,
-    connected: bool,
+pub enum NoteState {
+    PreHandshake,
+    Ready,
 }
 
-pub fn status() -> Result<Status, ()> {
-    serde_json_core::from_str(
-        r#"{
-    "status":    "{normal}",
-    "usb":       true,
-    "storage":   8,
-    "time":      1599684765,
-    "connected": "true"
-    }"#,
-    )
-    .map_err(|_| ())
-    .map(|(a, _)| a)
+pub struct Note {
+    i2c: hal::i2c::I2c,
+    addr: u8,
+    state: NoteState,
 }
+
+impl Note {
+    pub fn new(i2c: hal::i2c::I2c) -> Note {
+        Note {
+            i2c,
+            addr: 0x17,
+            state: NoteState::PreHandshake,
+        }
+    }
+
+    pub fn reset(&mut self) {
+    }
+}
+
+// use serde::Deserialize;
+
+// #[derive(Deserialize)]
+// pub struct Status {
+//     status: heapless::String<10>,
+//     usb: bool,
+//     storage: usize,
+//     time: u64,
+//     connected: bool,
+// }
+
+// pub fn status() -> Result<Status, ()> {
+//     serde_json_core::from_str(
+//         r#"{
+//     "status":    "{normal}",
+//     "usb":       true,
+//     "storage":   8,
+//     "time":      1599684765,
+//     "connected": "true"
+//     }"#,
+//     )
+//     .map_err(|_| ())
+//     .map(|(a, _)| a)
+// }
 
 #[cfg(test)]
 mod tests {}
