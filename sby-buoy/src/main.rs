0855fcf42d8a267753c69a47ccb8cab6069e1369
Gaute Hope <gaute.hope@met.no>
2021-10-19 14:35:15 +0200

    setup connection to hub

--- sby-buoy/src/main.rs
+++ sby-buoy/src/main.rs
@@ -8,22 +8,23 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
                      // use panic_itm as _; // logs messages over ITM; requires ITM support
                      // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
+#[allow(unused_imports)]
+use defmt::{debug, error, info, trace, warn};
+
 use ambiq_hal as hal;
 use cortex_m_rt::entry;
 use hal::prelude::*;
+use notecard::Note;
 
 mod defmt_uart;
 use defmt_uart::{UartLogger, LOGGER};
 
-#[allow(unused_imports)]
-use defmt::{debug, error, info, trace, warn};
-
-use notecard::Note;
+mod note;
 
 #[entry]
 fn main() -> ! {
-    // Set the clock frequency.
     unsafe {
+        // Set the clock frequency.
         halc::am_hal_clkgen_control(
             halc::am_hal_clkgen_control_e_AM_HAL_CLKGEN_CONTROL_SYSCLK_MAX,
             0 as *mut c_void,
@@ -39,11 +40,14 @@ fn main() -> ! {
 
     let mut dp = hal::pac::Peripherals::take().unwrap();
     let core = hal::pac::CorePeripherals::take().unwrap();
+    let mut delay = hal::delay::Delay::new(core.SYST, &mut dp.CLKGEN);
 
     let pins = hal::gpio::Pins::new(dp.GPIO);
+    let mut led = pins.d13.into_push_pull_output();
 
-    let mut delay = hal::delay::Delay::new(core.SYST, &mut dp.CLKGEN);
     let serial = hal::uart::Uart0::new(dp.UART0, pins.tx0, pins.rx0);
+
+    // Set up defmt over serial
     unsafe {
         LOGGER = Some(UartLogger { uart: serial });
     }
@@ -51,9 +55,7 @@ fn main() -> ! {
     // Initialize notecard
     let i2c = hal::i2c::I2c::new(dp.IOM4, pins.d15, pins.d14);
     let mut note = Note::new(i2c);
-    note.initialize().ok();
-
-    let mut led = pins.d13.into_push_pull_output();
+    note.initialize().expect("could not initialize notecard.");
 
     defmt::info!("Waiting to start..!");
     delay.delay_ms(2000u32);
@@ -69,8 +71,23 @@ fn main() -> ! {
         error!("notecard not found!");
     }
 
+    info!("note: card.time");
+    info!("note: time: {:?}", note.card().time().unwrap().wait());
+
+    note.hub()
+        .set(Some("com.vetsj.gaute.eg:sby"), None, None, Some("cain"))
+        .unwrap()
+        .wait()
+        .ok();
+
     delay.delay_ms(1000u32);
     info!("done: looping.");
+
+    warn!("note: logging startup");
+    note.hub().log("cain starting up!", true, true).unwrap().wait().ok();
+
+    warn!("note: syncing");
+    note.hub().sync().unwrap().wait().ok();
 
     loop {
         delay.delay_ms(2000u32);
@@ -80,11 +97,12 @@ fn main() -> ! {
         info!("querying status..");
         info!("status: {:?}", note.card().status().unwrap().wait());
 
+        info!("querying sync status..");
+        info!("status: {:?}", note.hub().sync_status().unwrap().wait());
         // Toggle LEDs
         led.toggle().unwrap();
     }
 }
 
 #[cfg(test)]
-mod tests {
-}
+mod tests {}
