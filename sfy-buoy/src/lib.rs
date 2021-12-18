ce155691c0bcbd79f5fc4d07e1bc7365a1b5379b
Gaute Hope <gaute.hope@met.no>
2021-12-18 08:37:41 +0100

    clean up main

--- sfy-buoy/src/lib.rs
+++ sfy-buoy/src/lib.rs
@@ -1,3 +1,4 @@
+#![feature(derive_default_enum)]
 #![cfg_attr(not(test), no_std)]
 
 #[allow(unused_imports)]
@@ -6,22 +7,111 @@ use defmt::{debug, error, info, trace, warn};
 // we use this for defs of sinf etc.
 extern crate cmsis_dsp;
 
+use core::fmt::Debug;
+use ambiq_hal::rtc::Rtc;
+use chrono::NaiveDateTime;
+use embedded_hal::blocking::{
+    delay::DelayMs,
+    i2c::{Read, Write, WriteRead},
+};
+
 pub mod note;
 pub mod waves;
 
-#[derive(Default, Debug, defmt::Format)]
+const LOCATION_DIFF: i64 = 1 * 60_000; // ms
+
+pub enum LocationState {
+    Trying(i64),
+    Retrieved(i64),
+}
+
 pub struct Location {
     pub lat: f32,
     pub lon: f32,
     pub time: u32,
 
-    pub retrived: Option<u32>,
-    pub last_tried: Option<u32>,
+    pub state: LocationState,
 }
 
+impl Location {
+    pub fn new() -> Location {
+        Location {
+            lat: 0.0,
+            lon: 0.0,
+            time: 0,
+            state: LocationState::Trying(-999),
+        }
+    }
+
+    pub fn check_retrieve<T: Read + Write>(
+        &mut self,
+        rtc: &mut Rtc,
+        delay: &mut impl DelayMs<u16>,
+        note: &mut note::Notecarrier<T>,
+    ) -> Result<(), notecard::NoteError> {
+        use notecard::card::res::Location;
+        use LocationState::*;
+
+        let now = rtc.now().timestamp_millis();
+
+        match self.state {
+            Retrieved(t) | Trying(t) if (now - t) > LOCATION_DIFF => {
+                // Try to get time and location
+                let gps = note.card().location()?.wait(delay)?;
+
+                info!("Location: {:?}", gps);
+                if let Location {
+                    lat: Some(lat),
+                    lon: Some(lon),
+                    time: Some(time),
+                    ..
+                } = gps
+                {
+                    info!("got time and location, setting RTC.");
+
+                    self.lat = lat;
+                    self.lon = lon;
+                    self.time = time;
+                    self.state = Retrieved(time as i64);
+
+                    rtc.set(NaiveDateTime::from_timestamp(time as i64, 0));
+                }
+            }
+            _ => (),
+        }
+
+        Ok(())
+    }
+}
+
+const IMU_BUF_DIFF: i64 = 100; // ms
 
 #[derive(Default)]
 pub struct Imu {
     pub dequeue: heapless::Deque<note::AxlPacket, 60>,
-    pub last_poll: u32,
+    pub last_poll: i64,
+}
+
+impl Imu {
+    pub fn check_retrieve<E: Debug, I: Write<Error = E> + WriteRead<Error = E>>(
+        &mut self,
+        rtc: &mut Rtc,
+        waves: &mut waves::Waves<I>,
+    ) -> Result<(), E> {
+        let now = rtc.now().timestamp_millis();
+
+        if (now - self.last_poll) > IMU_BUF_DIFF {
+            info!("Polling IMU..");
+            self.last_poll = now;
+
+            waves.read_and_filter()?;
+
+            if waves.axl.is_full() {
+                let pck = waves.take_buf(now as u32)?;
+                self.dequeue.push_back(pck).unwrap(); // TODO: fix
+            }
+        }
+
+        Ok(())
+    }
 }
--- sfy-buoy/src/main.rs
+++ sfy-buoy/src/main.rs
@@ -11,9 +11,9 @@ use defmt::{debug, error, info, println, trace, warn};
 use cortex_m_rt::entry;
 
 use ambiq_hal::{self as hal, prelude::*};
-use chrono::{NaiveDate, NaiveDateTime};
 use defmt_rtt as _;
 use hal::i2c;
+use chrono::NaiveDate;
 
 use sfy::note::Notecarrier;
 use sfy::waves::Waves;
@@ -58,71 +58,22 @@ fn main() -> ! {
     info!("Setting up IMU..");
     let mut waves = Waves::new(bus.acquire_i2c()).unwrap();
     waves.take_buf(rtc.now().timestamp_millis() as u32).unwrap(); // set timestamp.
-    waves.enable_fifo(&mut delay).unwrap();
 
-    let mut location = sfy::Location::default();
-    const LOCATION_DIFF: u32 = 1 * 60_000; // ms
-
+    // Subsystem state
+    let mut location = sfy::Location::new();
     let mut imu = sfy::Imu::default();
-    const IMU_BUF_DIFF: u32 = 100; // ms
+
+    info!("Enable IMU.");
+    waves.enable_fifo(&mut delay).unwrap();
 
     info!("Entering main loop");
 
     loop {
         led.toggle().unwrap();
 
-        // Get now from RTC.
-        let now = rtc.now().timestamp_millis();
+        location.check_retrieve(&mut rtc, &mut delay, &mut note).unwrap();
+        imu.check_retrieve(&mut rtc, &mut waves).unwrap();
 
-        // Retrieve location and time if necessary
-        if location
-            .retrived
-            .map(|r| (now - r as i64) > LOCATION_DIFF as i64)
-            .unwrap_or(false)
-        {
-            if location
-                .last_tried
-                .map(|r| (now - r as i64) > LOCATION_DIFF as i64)
-                .unwrap_or(false)
-            {
-                use notecard::card::res::Location;
-
-                location.last_tried = Some(now as u32);
-
-                // Try to get time and location
-                let gps = note.card().location().unwrap().wait(&mut delay).unwrap();
-                info!("Location: {:?}", gps);
-
-                if let Location {
-                    lat: Some(lat),
-                    lon: Some(lon),
-                    time: Some(time),
-                    ..
-                } = gps
-                {
-                    info!("got time and location, setting RTC.");
-
-                    location.lat = lat;
-                    location.lon = lon;
-                    location.time = time;
-                    location.retrived = Some(time);
-
-                    rtc.set(NaiveDateTime::from_timestamp(time as i64, 0));
-                }
-            }
-        }
-
-        if (now - imu.last_poll as i64) > IMU_BUF_DIFF as i64 {
-            info!("Polling IMU..");
-            imu.last_poll = now as u32;
-
-            waves.read_and_filter().unwrap();
-
-            if waves.axl.is_full() {
-                let pck = waves.take_buf(now as u32).unwrap();
-                imu.dequeue.push_back(pck).unwrap();
-            }
-        }
 
         // Check if IMU queue is full
         if imu.dequeue.is_full() { // or IN_DRAINING_QUEUE
