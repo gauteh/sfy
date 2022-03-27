609e36dd170a609c94c4463d7caba24e98e67079
Gaute Hope <gaute.hope@met.no>
2022-03-27 16:16:04 +0200

    jest: mock in src/

--- sfy-dashboard/jest.config.js
+++ sfy-dashboard/jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
   moduleDirectories: [ "node_modules", "src" ],
   moduleNameMapper: {
     "src/(.*)": "<rootDir>/src/$1",
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/__mocks__/fileMock.js",
     "\\.(css|less|scss)$": "identity-obj-proxy"
   }
 };
