ea0ab6cdbfa8a84bbccaf42fd953ab7bb4df88ca
Gaute Hope <gaute.hope@met.no>
2022-04-20 14:39:07 +0200

    prc: add setup.py

---
+++ sfy-processing/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(
+    name='sfy-processing',
+    version='0.1.0',
+    description='Processing tools for SFY',
+    author='Gaute Hope',
+    author_email='gauteh@met.no',
+    url='http://github.com/gauteh/sfy',
+    packages=setuptools.find_packages(),
+    include_package_data=False,
+    setup_requires=['setuptools_scm'],
+)
