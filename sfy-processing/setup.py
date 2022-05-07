6d83cb8c7662ca8654fffc40034c4e99bf72149f
Gaute Hope <gaute.hope@met.no>
2022-05-07 10:51:37 +0200

    py: move to pyproject

--- sfy-processing/setup.py
+++ sfy-processing/setup.py
@@ -1,21 +1,3 @@
-#!/usr/bin/env python
+from setuptools import setup
 
-import setuptools
-
-setuptools.setup(
-    name='sfy-processing',
-    version='0.1.0',
-    description='Processing tools for SFY',
-    author='Gaute Hope',
-    author_email='gauteh@met.no',
-    url='http://github.com/gauteh/sfy',
-    packages=setuptools.find_packages(),
-    include_package_data=False,
-    setup_requires=['setuptools_scm'],
-    entry_points={
-        'console_scripts': [
-            'sfydata=sfy.cli.sfydata:sfy'
-            ]
-        }
-
-)
+setup()
