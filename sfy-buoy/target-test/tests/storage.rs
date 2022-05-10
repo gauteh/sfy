079ed8acde5278699368a9ff5645287c12674b9f
Gaute Hope <gaute.hope@met.no>
2022-05-10 15:28:29 +0200

    test: acuire sd card ok

--- sfy-buoy/target-test/tests/storage.rs
+++ sfy-buoy/target-test/tests/storage.rs
@@ -12,7 +12,9 @@ mod tests {
     #[allow(unused)]
     use defmt::{assert, assert_eq, info};
     use hal::spi::{Freq, Spi};
+    use hal::prelude::*;
     use embedded_hal::spi;
+    use embedded_sdmmc::SdMmcSpi;
 
     struct State {
         // note: Notecarrier<hal::i2c::Iom2>,
@@ -20,6 +22,8 @@ mod tests {
         delay: hal::delay::Delay,
         #[allow(unused)]
         rtc: hal::rtc::Rtc,
+
+        sd: SdMmcSpi<hal::spi::Spi0, hal::gpio::pin::P35<{ hal::gpio::pin::Mode::Output }>>,
     }
 
     #[init]
@@ -30,18 +34,23 @@ mod tests {
         let pins = hal::gpio::Pins::new(dp.GPIO);
 
         let rtc = hal::rtc::Rtc::new(dp.RTC, &mut dp.CLKGEN);
+        let delay = hal::delay::Delay::new(core.SYST, &mut dp.CLKGEN);
 
-        let mut delay = hal::delay::Delay::new(core.SYST, &mut dp.CLKGEN);
-        let spi = Spi::new(dp.IOM0, pins.d11, pins.d12, pins.d13, Freq::F100kHz, spi::MODE_0);
+        defmt::info!("Setting up SPI");
+        let spi = Spi::new(dp.IOM0, pins.d12, pins.d13, pins.d11, Freq::F100kHz, spi::MODE_0);
 
-        // defmt::info!("Setting up notecarrier");
-        // let note = Notecarrier::new(i2c, &mut delay).unwrap();
+        let cs = pins.a14.into_push_pull_output();
 
-        State { delay, rtc }
+        defmt::info!("Setting up SD SPI card driver");
+        let sd = SdMmcSpi::new(spi, cs);
+
+        State { delay, rtc, sd }
     }
 
     #[test]
-    fn ping_sd_card(s: &mut State) {
+    fn init_sd_card(s: &mut State) {
+        defmt::info!("init SD");
+        let _block = s.sd.acquire().unwrap();
     }
 }
 
