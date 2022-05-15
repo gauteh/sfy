7bca8735e2c083e572821f8899d718a7824b2ec2
Gaute Hope <gaute.hope@met.no>
2022-05-15 16:27:11 +0200

    storage: exclusive file handle

--- sfy-buoy/src/storage.rs
+++ sfy-buoy/src/storage.rs
@@ -5,17 +5,20 @@
 
 use chrono::{Datelike, NaiveDateTime, Timelike};
 use core::sync::atomic::Ordering;
-use embedded_hal::spi;
-use embedded_sdmmc::{SdMmcError, SdMmcSpi, TimeSource, Timestamp};
+use embedded_sdmmc::{
+    BlockDevice, Controller, Error as GenericSdMmcError, File, Mode, SdMmcError, SdMmcSpi,
+    TimeSource, Timestamp, Volume, VolumeIdx,
+};
 
-use ambiq_hal::gpio::pin::{Mode, P35 as CS};
-use ambiq_hal::spi::{Freq, Spi0 as Spi};
+use ambiq_hal::gpio::pin::{Mode as SpiMode, P35 as CS};
+use ambiq_hal::spi::Spi0 as Spi;
 
 use crate::axl::AxlPacket;
 use crate::COUNT;
 
 pub enum StorageErr {
     SdMmcErr(SdMmcError),
+    GenericSdMmmcErr(GenericSdMmcError<SdMmcError>),
 }
 
 impl From<SdMmcError> for StorageErr {
@@ -24,26 +27,97 @@ impl From<SdMmcError> for StorageErr {
     }
 }
 
+impl From<embedded_sdmmc::Error<SdMmcError>> for StorageErr {
+    fn from(e: embedded_sdmmc::Error<SdMmcError>) -> Self {
+        StorageErr::GenericSdMmmcErr(e)
+    }
+}
+
+/// A FileHandle that requires exclusive access to the Controller and Volume. Not a big loss
+/// for us, but it allows use to make _more_ sure the file-handle is closed, and not
+/// accidently left open because of early-returns `(?)`.
+struct FileHandle<'c, 'v, D: BlockDevice, T: TimeSource> {
+    ctrl: &'c mut Controller<D, T>,
+    vol: &'v mut Volume,
+    file: Option<File>,
+}
+
+impl<'c, 'v, D: BlockDevice, T: TimeSource> FileHandle<'c, 'v, D, T> {
+    pub fn new(ctrl: &'c mut Controller<D, T>, vol: &'v mut Volume, file: File) -> Self {
+        FileHandle {
+            ctrl,
+            vol,
+            file: Some(file),
+        }
+    }
+
+    // pub fn open_in_dir(
+
+    pub fn close(self) {
+        drop(self);
+    }
+
+    pub fn read(&mut self, buf: &mut [u8]) {
+        let file = self.file.as_mut().unwrap();
+        self.ctrl.read(&self.vol, file, buf);
+    }
+
+    pub fn write(&mut self, buf: &[u8]) {
+        let file = self.file.as_mut().unwrap();
+        self.ctrl.write(&mut self.vol, file, buf);
+    }
+}
+
+impl<D: BlockDevice, T: TimeSource> Drop for FileHandle<'_, '_, D, T> {
+    fn drop(&mut self) {
+        let file = self.file.take();
+
+        if let Some(file) = file {
+            self.ctrl.close_file(&self.vol, file);
+        }
+    }
+}
+
+const ID_FILE: &'static str = "sfy.id";
+
 pub struct Storage {
-    sd: SdMmcSpi<Spi, CS<{ Mode::Output }>>,
+    sd: SdMmcSpi<Spi, CS<{ SpiMode::Output }>>,
     /// Last written ID.
     current_id: u32,
 }
 
 impl Storage {
-    pub fn open(spi: Spi, cs: CS<{ Mode::Output }>) -> Result<Storage, StorageErr> {
+    pub fn open(spi: Spi, cs: CS<{ SpiMode::Output }>) -> Result<Storage, StorageErr> {
         // Get last id (or create file with 0, verify it's free, or scan)
         defmt::info!("Opening SD card..");
 
         let mut sd = SdMmcSpi::new(spi, cs);
+        // TODO: Re-clock SPI
 
         defmt::info!("Initialize SD-card..");
         {
             let block = sd.acquire()?;
-
             let sz = block.card_size_bytes()? / 1024_u64.pow(2);
-
             defmt::info!("SD card size: {} mb", sz);
+
+            let mut c = Controller::new(block, CountClock);
+            let mut v = c.get_volume(VolumeIdx(0))?;
+
+            let root = c.open_root_dir(&v)?;
+
+            let idf = c.open_file_in_dir(&mut v, &root, ID_FILE, Mode::ReadOnly);
+
+            match idf {
+                Ok(f) => {
+                    let mut buf = [0u8; 128];
+                    let mut fh = FileHandle::new(&mut c, &mut v, f);
+                    fh.read(&mut buf);
+
+                    Ok(0)
+                }
+                Err(GenericSdMmcError::FileNotFound) => Ok(0),
+                Err(e) => Err(e),
+            }?;
         }
 
         Ok(Storage { sd, current_id: 0 })
