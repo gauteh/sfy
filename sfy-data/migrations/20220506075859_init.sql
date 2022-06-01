15c9ee0d03eeb57758ba5c8e49ae39a7ff276061
Gaute Hope <gaute.hope@met.no>
2022-06-01 18:47:09 +0200

    don't modify initial migration

--- sfy-data/migrations/20220506075859_init.sql
+++ sfy-data/migrations/20220506075859_init.sql
@@ -1,3 +1,4 @@
+-- Add migration script here
 CREATE TABLE IF NOT EXISTS buoys (dev TEXT, name TEXT, PRIMARY KEY (dev));
 
 CREATE TABLE IF NOT EXISTS events (dev TEXT, event TEXT, received UNSIGNED BIGINT NOT NULL, data BLOB, PRIMARY KEY (dev, event));
