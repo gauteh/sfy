b58ca1e66bc939203c20b59fdf6f8ea958379917
Gaute Hope <gaute.hope@met.no>
2022-06-09 11:24:18 +0200

    add more descriptions

--- sfy-processing/sfy/timeseries.py
+++ sfy-processing/sfy/timeseries.py
@@ -23,26 +23,64 @@ class AxlTimeseries:
         ds = xr.Dataset(
             data_vars={
                 'z':
-                xr.Variable(('time'), self.z.astype(np.float32)),
+                xr.Variable(
+                    ('time'),
+                    self.z.astype(np.float32),
+                    attrs={
+                        'unit': 'm -s^2',
+                        'description':
+                        'Vertical acceleration (including gravity)'
+                    }),
                 'x':
-                xr.Variable(('time'), self.x.astype(np.float32)),
+                xr.Variable(
+                    ('time'),
+                    self.x.astype(np.float32),
+                    attrs={
+                        'unit': 'm -s^2',
+                        'description': 'Horizontal x-axis acceleration'
+                    }),
                 'y':
-                xr.Variable(('time'), self.y.astype(np.float32)),
+                xr.Variable(
+                    ('time'),
+                    self.y.astype(np.float32),
+                    attrs={
+                        'unit': 'm -s^2',
+                        'description': 'Horizontal y-axis acceleration'
+                    }),
                 'lon':
-                xr.Variable(('position_time'), np.array(self.lons, dtype=np.float64)),
+                xr.Variable(
+                    ('position_time'),
+                    np.array(self.lons, dtype=np.float64),
+                    attrs={
+                        'units': "degrees_east",
+                        'standard_name': "longitude",
+                        'long_name': "longitude"
+                    }),
                 'lat':
-                xr.Variable(('position_time'), np.array(self.lats, dtype=np.float64)),
+                xr.Variable(
+                    ('position_time'),
+                    np.array(self.lats, dtype=np.float64),
+                    attrs={
+                        'units': "degrees_north",
+                        'standard_name': "latitude",
+                        'long_name': "latitude"
+                    }),
                 'added':
                 xr.Variable(('received'), [
                     np.datetime64(int(s.timestamp() * 1000.), 'ms')
                     for s in self.added_times
-                ]),
+                ],
+                            attrs={
+                                'description':
+                                'Time package was added to notecard.'
+                            }),
                 'storage_id':
                 xr.Variable(('received'),
                             np.array([
                                 id if id is not None else np.nan
                                 for id in self.storage_ids
-                            ])),
+                            ]),
+                            attrs={'description': 'ID of packge on SD-card'}),
             },
             coords={
                 'time':
@@ -61,7 +99,11 @@ class AxlTimeseries:
                 xr.Variable(('received'), [
                     np.datetime64(int(s.timestamp() * 1000.), 'ms')
                     for s in self.received_times
-                ]),
+                ],
+                            attrs={
+                                'description':
+                                'Time package was received by data-hub'
+                            }),
             },
             attrs={
                 'frequency': self.frequency,
