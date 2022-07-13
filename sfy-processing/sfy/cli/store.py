6bbaa97ba40d7496c828ce9eefd113d772ed950f
Gaute Hope <gaute.hope@met.no>
2022-07-13 19:56:00 +0200

    use new axl collection read bin

--- sfy-processing/sfy/cli/store.py
+++ sfy-processing/sfy/cli/store.py
@@ -8,7 +8,7 @@ import hashlib
 import json
 from datetime import datetime
 from sfy.hub import Hub
-from sfy.axl import Axl
+from sfy.axl import Axl, AxlCollection
 
 logger = logging.getLogger(__name__)
 
@@ -57,10 +57,7 @@ def put(dev, file, really, cont, start_id, stop_id):
     b = hub.buoy(dev)
     logger.info(f"Putting packages in {file} to {b}")
 
-    logger.info("Parsing collection..")
-    collection = subprocess.check_output(["sfypack", "--note", file])
-    collection = json.loads(collection)
-    logger.info(f"Read {len(collection)} packages.")
+    collection = AxlCollection.from_storage_file(b.name, b.dev, file).pcks
 
     packages = b.axl_packages_range()
 
@@ -71,7 +68,9 @@ def put(dev, file, really, cont, start_id, stop_id):
 
     uploaded = 0
 
-    for event in collection:
+    for new_p in collection:
+        event = json.loads(new_p.json())
+
         if start_id is not None:
             if event['body']['storage_id'] < start_id:
                 continue
@@ -80,40 +79,8 @@ def put(dev, file, really, cont, start_id, stop_id):
             if event['body']['storage_id'] > stop_id:
                 continue
 
-        time, lat, lon = event['body']['timestamp'], event['body'][
-            'lat'], event['body']['lon']
-        time_s = time / 1.e3
+        time = event['body']['timestamp']
 
-        event['device'] = "dev:" + b.dev[3:]
-        event['sn'] = b.name
-        event['received'] = time_s
-        event['when'] = int(time_s)
-        event['from_store'] = True
-        event['file'] = "axl.qo"
-        event['where_when'] = int(time_s)
-        event['where_lat'] = lat
-        event['where_lon'] = lon
-        event['where_timezone'] = 'UTC'
-        event['tower_when'] = int(time_s)
-        event['tower_lat'] = lat
-        event['tower_lon'] = lon
-        event['tower_timezone'] = 'UTC'
-
-        # simulate blues notecard JSON where default value is removed
-        for key in ['lon', 'lat', 'timestamp', 'position_time']:
-            if event['body'].get(key) == 0:
-                del event['body'][key]
-
-
-        # make event id
-        hash = hashlib.shake_256()
-        hash.update(str(event['body']).encode('utf-8'))
-        hash.update(event['payload'].encode('utf-8'))
-        hash = hash.hexdigest(length=int((36 - 4) / 2))
-        hash = f"{hash[:8]}-{hash[8:12]}-{hash[12:16]}-{hash[16:20]}-{hash[20:]}"
-        event['event'] = hash
-
-        new_p = Axl.parse(json.dumps(event))
         uri = f"{int(time):013d}-{event['event']}_axl.qo.json"
         logger.info(f"Event: {uri}, package: {new_p}")
 
