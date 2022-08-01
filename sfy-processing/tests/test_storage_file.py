5a68fe55c8bd962eba203931983cedde517038bb
Gaute Hope <gaute.hope@met.no>
2022-08-01 14:51:49 +0200

    test: skip if sfypack not installed

--- sfy-processing/tests/test_storage_file.py
+++ sfy-processing/tests/test_storage_file.py
@@ -1,11 +1,13 @@
 import pytest
 import numpy as np
 from datetime import datetime, timezone
+import shutil
 
 from sfy import hub
 from sfy.axl import AxlCollection
 from . import sfyhub
 
+@pytest.mark.skipif(shutil.which('sfypack') is None, reason = 'sfypack not installed')
 def test_parse_collection():
     f = '../sfy-buoy/tests/data/74.1'
     c = AxlCollection.from_storage_file('WAVEBUG04', '778', f)
