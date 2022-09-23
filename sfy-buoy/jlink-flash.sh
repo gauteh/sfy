0970b9713430bb0a86ddbcd5dc6b6b47ce8dcf4d
Gaute Hope <gaute.hope@met.no>
2022-09-23 09:57:38 +0200

    jlink-flash: work with any path

--- sfy-buoy/jlink-flash.sh
+++ sfy-buoy/jlink-flash.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-cd target/
+D=$(dirname $1)
+B=$(basename $1)
+cd ${D}
+
+echo "Flashing: ${B} from ${D}"
 
 JLinkExe -device AMA3B1KK-KBR -autoconnect 1 -if swd -speed 4000 <<EOF
 h
-loadbin sfy-buoy.bin 0x10000
+loadbin ${B} 0x10000
 r
 go
 EOF
