f13c739c96558e576941c33e9a099446807118b4
Gaute Hope <gaute.hope@met.no>
2022-10-16 12:44:29 +0200

    render everything with react-leaflet

--- sfy-dashboard3/src/components/buoy-index/BuoyMap.tsx
+++ sfy-dashboard3/src/components/buoy-index/BuoyMap.tsx
@@ -1,14 +1,30 @@
 import {Component, createRef} from 'react';
-import { MapContainer, TileLayer, useMap, Marker, Popup, Tooltip } from 'react-leaflet';
-// import L from 'leaflet';
-
+import { MapContainer, TileLayer, Marker, Popup, Tooltip } from 'react-leaflet';
+import L from 'leaflet';
 import {Buoy, OmbBuoy} from 'models';
 
 import './BuoyIndex.scss';
 import 'leaflet/dist/leaflet.css';
-// import marker_me from './marker-me.png';
-// import marker_icon from 'leaflet/dist/images/marker-icon-2x.png';
-// import marker_shadow from 'leaflet/dist/images/marker-shadow.png';
+import icon_me from './marker-me.png';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+const DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
+const myselfIcon =  L.icon({
+          iconUrl: icon_me,
+
+          iconSize:     [30, 38], // size of the icon
+          // shadowSize:   [50, 64], // size of the shadow
+          iconAnchor:   [15, 38], // point of the icon which will correspond to marker's location
+          // shadowAnchor: [4, 62],  // the same for the shadow
+          // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+        });
 
 const MAPBOX_TOKEN: string = 'pk.eyJ1IjoiZ2F1dGVoIiwiYSI6ImNreWZ2MWd4NjBxNnoyb3M4eWRqNjlmMGMifQ.m-5Q9BBf2yQxp1fGStxYRg';
 
@@ -17,20 +33,20 @@ interface Props {
 }
 
 interface State {
-  myself: [] | undefined;
+  myself?: Array<number>;
 }
 
 export class BuoyMap
   extends Component<Props, State>
 {
 
-  public state = { myself: undefined };
+  public state: State = { myself: undefined };
   map: any = undefined
 
   constructor(props: Props) {
     super(props);
 
-    // this.map = useMap();
+    this.map = createRef();
 
     if (navigator.geolocation) {
       navigator.geolocation.watchPosition(this.updateMyPosition);
@@ -39,24 +55,7 @@ export class BuoyMap
 
   public updateMyPosition = (position: any) => {
     console.log("Got new position:" + position);
-    // if (this.map != undefined) {
-    //   if (this.myselfMarker === undefined) {
-    //     const icon = L.icon({
-    //       iconUrl: marker_me,
-
-    //       iconSize:     [30, 38], // size of the icon
-    //       // shadowSize:   [50, 64], // size of the shadow
-    //       iconAnchor:   [15, 38], // point of the icon which will correspond to marker's location
-    //       // shadowAnchor: [4, 62],  // the same for the shadow
-    //       // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-    //     });
-
-    //     this.myselfMarker = L.marker([position.coords.latitude, position.coords.longitude], {icon: icon}).addTo(this.map);
-    //     this.myselfMarker.bindTooltip('Your position');
-    //   } else {
-    //     this.myselfMarker.setLatLng([position.coords.latitude, position.coords.longitude]);
-    //   }
-    // }
+    this.state.myself = [position.coords.latitude, position.coords.longitude];
   }
 
   public focus = (buoy: any) => {
@@ -65,47 +64,45 @@ export class BuoyMap
     this.map.flyTo([buoy.any_lat(), buoy.any_lon()], 11);
   }
 
-  public render() {
-    return (
-      <MapContainer className="container-fluid" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+  public BuoyMarker = (buoy: any) => {
 
+    return (
+    <Marker key={buoy.dev} position={[buoy.any_lat() as number, buoy.any_lon() as number]}>
+                <Tooltip>
+                  {buoy.sn} ({buoy.dev})
+                </Tooltip>
+              </Marker>
+            )
+  }
+
+  public render() {
+    // <TileLayer
+    //   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    //   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    // />
+
+    return (
+    <MapContainer ref={this.map} className="container-fluid" center={[60.11304848114283, 2.3882482939071434]} zoom={5}>
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+            attribution='Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+            url='https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}'
+            id='mapbox/light-v10'
+            accessToken={MAPBOX_TOKEN} />
 
         <TileLayer url='https://t1.openseamap.org/seamark/{z}/{x}/{y}.png' />
 
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        { this.props.buoys.filter((buoy) => buoy.any_lat() != undefined).map(this.BuoyMarker) }
+
+        { this.state.myself != undefined &&
+          <Marker key="myself" position={[this.state.myself[0], this.state.myself[1]]} icon={myselfIcon}>
+            <Tooltip>
+            Your position.
+            </Tooltip>
+          </Marker>
+        }
+
       </MapContainer>
     );
   }
 }
-
-
-      // <div id="map-container" className="container-fluid">
-      //   <div id="map"></div>
-      //   <MapContainer center={[60.11304848114283, 2.3882482939071434]} zoom={5}>
-      //     <TileLayer url='https://t1.openseamap.org/seamark/{z}/{x}/{y}.png' />
-      //   </MapContainer>
-      // </div>
-
-          // <TileLayer
-          //   attribution='Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
-          //   url='https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}'
-          //   id='mapbox/light-v10'
-          //   accessToken={MAPBOX_TOKEN} />
-
-          // {
-          // this.props.buoys.filter((buoy) => buoy.any_lat() != undefined).map((buoy) => {
-          //     <Marker position={[buoy.any_lat() as number, buoy.any_lon() as number]}>
-          //       <Tooltip>
-          //         {`${buoy.sn} (${buoy.dev})`}
-          //       </Tooltip>
-          //     </Marker>
-          //   } }
 
