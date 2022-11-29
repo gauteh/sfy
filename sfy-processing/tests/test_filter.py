786e2f8b192beea9bbf86e71f64d64c04aa9675d
Gaute Hope <gaute.hope@met.no>
2022-11-29 20:43:59 +0100

    test filters

--- sfy-processing/tests/test_filter.py
+++ sfy-processing/tests/test_filter.py
@@ -19,3 +19,74 @@ def test_fir():
     # plt.legend()
     # plt.show()
 
+def test_filter_coeffs():
+    f50 = eval(open('../sfy-buoy/src/firwin.25_208_coeff').read())
+    f50 = np.array(f50)
+
+    NTAP = 128      # Length of filter
+    CUTOFF = 25.    # Cut-off frequency for output
+    FREQ = 208.     # Input frequency
+    fir = sc.signal.firwin(NTAP, cutoff=CUTOFF, pass_zero='lowpass', fs = FREQ)
+
+    np.testing.assert_array_equal(fir, f50)
+
+    f20 = eval(open('../sfy-buoy/src/firwin.10_208_coeff').read())
+    f20 = np.array(f20)
+
+    NTAP = 128      # Length of filter
+    CUTOFF = 10.    # Cut-off frequency for output
+    FREQ = 208.     # Input frequency
+    fir = sc.signal.firwin(NTAP, cutoff=CUTOFF, pass_zero='lowpass', fs = FREQ)
+
+    np.testing.assert_array_equal(fir, f20)
+
+def test_fir_within_sin():
+    NTAP = 129      # Length of filter
+    CUTOFF = 25.    # Cut-off frequency for output
+    FREQ = 208.     # Input frequency
+    fir = sc.signal.firwin(NTAP, cutoff=CUTOFF, pass_zero='lowpass', fs = FREQ)
+
+    dt = 1. / FREQ
+    t = np.arange(0, 4096) * dt
+    s = 2 * np.sin(2 * t * 2 * np.pi)
+
+    sf = np.convolve(s, fir, mode='valid')
+    # sf = sf[NTAP//2:]
+
+    # import matplotlib.pyplot as plt
+    # plt.figure()
+    # plt.plot(np.arange(0,len(s)), s, '-x')
+    # plt.plot(np.arange(0,len(sf))+64, sf, '-x')
+    # plt.show()
+
+    # sf = sf[:]
+    s = s[64:-64]
+
+    np.testing.assert_almost_equal(sf, s, 4)
+
+def test_fir_outside_sin():
+    NTAP = 129      # Length of filter
+    CUTOFF = 20.    # Cut-off frequency for output
+    FREQ = 208.     # Input frequency
+    fir = sc.signal.firwin(NTAP, cutoff=CUTOFF, pass_zero='lowpass', fs = FREQ)
+
+    dt = 1. / FREQ
+    t = np.arange(0, 4096) * dt
+
+    s = 2 * np.sin(2 * t * 2 * np.pi)
+    s2 = 2 * np.sin(50 * t * 2 * np.pi)
+    s3 = 2 * np.sin(25 * t * 2 * np.pi)
+
+    sf = np.convolve(s+s2+s3, fir, mode='valid')
+    # sf = sf[NTAP//2:]
+
+    # import matplotlib.pyplot as plt
+    # plt.figure()
+    # plt.plot(np.arange(0,len(s)), s, '-x')
+    # plt.plot(np.arange(0,len(sf))+64, sf, '-x')
+    # plt.show()
+
+    # sf = sf[:]
+    s = s[64:-64]
+
+    np.testing.assert_almost_equal(sf, s, 2)
