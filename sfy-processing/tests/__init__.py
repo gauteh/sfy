f6e1f1572282d1ccd030d203cf09fb02ca770acb
Gaute Hope <gaute.hope@met.no>
2023-06-29 12:44:10 +0200

    tests: skip if no hub

--- sfy-processing/tests/__init__.py
+++ sfy-processing/tests/__init__.py
@@ -2,16 +2,16 @@ import pytest
 import os
 from sfy import hub
 
-def has_hub():
+def not_has_hub():
     API = os.getenv('SFY_SERVER')
     KEY = os.getenv('SFY_READ_TOKEN')
 
     if API is None or KEY is None:
-        return False
+        return True
 
-    return True
+    return False
 
-needs_hub = pytest.mark.skipif(not has_hub(), reason="No data hub must configured.")
+needs_hub = pytest.mark.skipif(not_has_hub(), reason="No data hub must configured.")
 
 @pytest.fixture
 def sfyhub(tmpdir):
