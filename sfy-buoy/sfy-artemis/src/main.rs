286e5af932c1e0ec544d33a10f10d7333596adb1
Stian Vikanes <s.vikanes@outlook.com>
2023-06-29 13:45:17 +0200

    change loop_delay

--- sfy-buoy/sfy-artemis/src/main.rs
+++ sfy-buoy/sfy-artemis/src/main.rs
@@ -296,7 +296,7 @@ fn main() -> ! {
 
         // XXX: This needs to be adapted to frequency, and queue length. Maybe just remove when we
         // have the remaining space check? Check after Hjeltefjorden deployment.
-        const LOOP_DELAY: u32 = 5 * 40_000;
+        const LOOP_DELAY: u32 = 14 * 20_000;
         const SHORT_LOOP_DELAY: u32 = 30_000;
 
         // Process data and communication for the Notecard.
