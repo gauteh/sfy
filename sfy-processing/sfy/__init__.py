58f2811bf01877b32c66fee5487b6fae04074200
Gaute Hope <gauteh@met.no>
2023-08-09 10:41:48 +0200

    add version fn

--- sfy-processing/sfy/__init__.py
+++ sfy-processing/sfy/__init__.py
@@ -1,3 +1,21 @@
 from . import axl, signal, hub, xr
 
 
+def version():
+    """
+    Return git version if available.
+    """
+    import os.path
+    from subprocess import check_output, DEVNULL
+
+    path = os.path.dirname(__file__)
+    args = [
+        "git", "-C", path, "describe", "--tags", "--abbrev=7", "--dirty",
+        "--broken", "--always",
+    ]
+
+    try:
+        version = check_output(args, cwd=path, stderr=DEVNULL).decode().strip()
+        return version
+    except:
+        return None
