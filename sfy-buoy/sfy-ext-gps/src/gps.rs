c790e7042477be803625faca557da9d58fc987df
Gaute Hope <gauteh@met.no>
2024-09-26 11:12:21 +0200

    rtk: docker+makefile

--- sfy-buoy/sfy-ext-gps/src/gps.rs
+++ sfy-buoy/sfy-ext-gps/src/gps.rs
@@ -1,3 +1,6 @@
+//! UBLOX RTK GPS interface
+//!
+//! Probably need to communicate over UART to avoid knowing I2C telegram length in advance.
 #[allow(unused_imports)]
 use defmt::{debug, error, info, println, trace, warn};
 use heapless::Vec;
