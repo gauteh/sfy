40065e8288d8ee7f99369dc6cd6b548139e40b95
Gaute Hope <gauteh@met.no>
2024-09-27 18:42:30 +0200

    sparkfun suggestions: https://github.com/sparkfun/SparkFun_u-blox_GNSS_v3/issues/67

--- sfy-buoy/sfy-ext-gps/ext-gps-mod/demo_gps.cpp
+++ sfy-buoy/sfy-ext-gps/ext-gps-mod/demo_gps.cpp
@@ -37,38 +37,30 @@ void setup_gps() {
 
     return;
 
-    // power things down
     delay(500);
   } else{
     Serial.println(F("success starting GNSS"));
   }
 
+  // factory reset (no need to do this every time, only once).
+  gnss.factoryDefault();
+  delay(5000);
+
   uint8_t ok = gnss.setI2COutput(COM_TYPE_UBX); //Turn off NMEA noise
   if (ok) ok = gnss.setI2CInput(COM_TYPE_UBX);
   if (ok) ok = gnss.setUART1Output(0);
   if (ok) ok = gnss.setUART1Input(0);
-  if (ok) ok = gnss.setUART2Output(0);
-  /* if (ok) ok = gnss.setUART2Input(COM_TYPE_SPARTN); //Be sure SPARTN input is enabled */
-  /* if (ok) ok = gnss.setDGNSSConfiguration(SFE_UBLOX_DGNSS_MODE_FIXED); // Set the differential mode - ambiguities are fixed whenever possible */
   if (ok) ok = gnss.setNavigationFrequency(1); //Set output in Hz.
-  /* if (ok) ok = myGNSS.setVal8(UBLOX_CFG_SPARTN_USE_SOURCE, 0); // Use "IP" source, not L-Band. We are injecting raw SPARTN, not PMP */
-  /* if (ok) ok = myGNSS.setVal8(UBLOX_CFG_MSGOUT_UBX_RXM_COR_I2C, 1); // Enable UBX-RXM-COR messages on I2C */
 
-  // now we know that we can talk to the gnss
-  /* delay(100); */
-
-  // If we are going to change the dynamic platform model, let's do it here.
-  // Possible values are:
-  // PORTABLE,STATIONARY,PEDESTRIAN,AUTOMOTIVE,SEA,AIRBORNE1g,AIRBORNE2g,AIRBORNE4g,WRIST,BIKE
-  /* if (!gnss.setDynamicModel(DYN_MODEL_STATIONARY)){ */
-  /*   Serial.println(F("GNSS could not set dynamic model")); */
-  /* } */
+  Serial.print(F("GPS setup flag: "));
+  Serial.println(ok);
 
   /* gnss.setAutoPVT(true); */
   gnss.setAutoPVTcallbackPtr(&getPVT); // Enable automatic NAV PVT messages with callback to printPVTdata so we can watch the carrier solution go to fixed
 }
 
 void loop_gps() {
+  // TODO: move these to PPS?
   gnss.checkUblox();
   gnss.checkCallbacks();
 }
@@ -77,6 +69,10 @@ void loop_gps() {
 void pps() {
   // pps pulse interrupt: should be triggered at the start of every gps second.
   pps_ts = micros();
+
+  Serial.println("GNSS: PPS!");
+
+  // TODO: checkUblox + checkCallbacks
 }
 
 # endif
