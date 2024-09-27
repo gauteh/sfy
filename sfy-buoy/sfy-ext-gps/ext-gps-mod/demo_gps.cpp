69df777d2a8c89db1a366b30c208fda70c0ef17e
Gaute Hope <gauteh@met.no>
2024-09-27 11:13:31 +0200

    demo gps

--- sfy-buoy/sfy-ext-gps/ext-gps-mod/demo_gps.cpp
+++ sfy-buoy/sfy-ext-gps/ext-gps-mod/demo_gps.cpp
@@ -1,12 +1,38 @@
 # ifndef GPS
 # include "gps.h"
 
+static uint32_t last = 0;
+# define RATE 20 // Hz
+
+std::vector<GpsM> msgs;
+
 void setup_gps() {
   Serial.println(F("GPS: DEMO GPS initiated."));
 }
 
 void loop_gps() {
+  if (micros() - last > (1000000. / RATE)) {
+
+    pps(); // trigger fake PPS in DEMO
+    delay(100); // fake delay between PPS, and read sample
+
+    last = micros();
+    Serial.println(F("DEMOGPS: New sample"));
+
+    // push new measurement.
+    GpsM m;
+    m.cputime = micros();
+    m.ppsdiff = m.cputime - m.ppsdiff;
+    m.gpstime = 0;
+
+    msgs.push_back(m);
+
+  }
 }
 
+void pps() {
+  // pps pulse interrupt detected
+  pps_ts = micros();
+}
 
 # endif
--- sfy-buoy/sfy-ext-gps/ext-gps-mod/gps.h
+++ sfy-buoy/sfy-ext-gps/ext-gps-mod/gps.h
@@ -1,13 +1,25 @@
 # ifndef GPS_H
 # define GPS_H
 # include <SparkFun_u-blox_GNSS_v3.h> //http://librarymanager/All#SparkFun_u-blox_GNSS_v3
+#include <vector>
                                       //
-const uint32_t myLBandFreq = 1556290000; // Uncomment this line to use the US SPARTN 1.8 service
+//const uint32_t myLBandFreq = 1556290000; // Uncomment this line to use the US SPARTN 1.8 service
 //const uint32_t myLBandFreq = 1545260000; // Uncomment this line to use the EU SPARTN 1.8 service
 
 #define OK(ok) (ok ? F("  ->  OK") : F("  ->  ERROR!")) // Convert uint8_t into OK/ERROR
+                                                        //
+
+static uint32_t pps_ts = 0;
+
+typedef struct GpsM_t {
+    uint64_t cputime = 0;
+    int64_t  ppsdiff = 0;
+    uint64_t gpstime = 0;
+    double lat = 0.0;
+} GpsM;
 
 void setup_gps();
 void loop_gps();
+void pps();
 
 # endif
